38681f17a4eca34f27908dc38cc52c7b2f3c8035
Brendan Morrissey <brendanmorrissey34@gmail.com>
2025-09-10 20:27:30 -0400

    doing the thing

--- pages/CampusLandingPage.js
+++
@@ -1,6 +0,0 @@
-'use client';
-import CampusMapView from '../../components/CampusMapView';
-
-export default function CampusLandingPage() {
-  return <CampusMapView src="/BuildingMaps/(Campus)/Campus.svg" />;
-}
