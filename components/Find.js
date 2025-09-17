33e880b795388602cdb856670712adc5cb13c98f
Justin <jmccabe1@ggc.edu>
2025-09-17 06:04:50 -0400

    buildings help and style

--- components/Find.js
+++ components/Find.js
@@ -2,12 +2,16 @@
 const maxCharsAllowed=30;
 
 export default function Find(){
+    const onHelpClick=function(){
+        alert("you clicked help");
+    }
+
     return (
         <div className="d-flex align-items-center" style={{gap: "var(--justin-globe-gap)"}}>
     <label htmlFor="findlabel" className="h4 mb-0" style={{fontFamily: "var(--justin-globe1)",color: "var(--justin-globe1-color)"}}>Find: </label>
     <input id="findInput" className="form-control w-100" placeholder="AEC,gameroom,library" maxLength={maxCharsAllowed} style={{width: "var(--justin-globe-inputBarSize)"}}></input>
     <button id="findInputButton" className="btn btn-primary">Find</button>
-    <button id="helpButton" className="btn btn-secondary">Help</button>
+    <button id="helpButton" className="btn btn-secondary" onClick={onHelpClick}>Help</button>
     </div>
     );
 }
